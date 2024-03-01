0bf850b2950b0345d9a8963144c9ef13182adb15
Greg Valainis <valainisgt@tutanota.com>
2024-02-29 20:15:37 -0500

    Use correct property name

--- Src/Private/Import-BitsTransferModule.ps1
+++ Src/Private/Import-BitsTransferModule.ps1
@@ -7,7 +7,7 @@ function Import-BitsTransferModule {
     param ( )
     process {
 
-        $bitsTransferPath = (Get-LabHostDefault).BitsPath;
+        $bitsTransferPath = (Get-LabHostDefault).BitsTransferPath;
         Remove-Module -Name 'Microsoft.BackgroundIntelligentTransfer.Management.Interop' -ErrorAction SilentlyContinue;
         $bitsTransferModule = Import-Module -Name $bitsTransferPath -Force -Scope Global -PassThru -Verbose:$false;
         Write-Verbose -Message ($localized.LoadedModuleVersion -f 'Bits', $bitsTransferModule.Version);
